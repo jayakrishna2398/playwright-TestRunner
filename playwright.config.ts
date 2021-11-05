7b84a6e1620398fb4853314bb09dc3a163eec5c8
jayakrishna2398 <jayakrishnakrish1998@gmail.com>
2021-11-05 16:26:36 +0530

    RELEASE: github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,11 @@ const config: PlaywrightTestConfig = {
         use:{
             headless: false,
             channel: "chrome",
-            screenshot: "off",
-            video: "off"
+            screenshot: "only-on-failure",
+            video: "retain-on-failure"
             //slowMo: "1000"   //This will slow down the execution while testing
         },
-        testMatch: ["github.test.ts"],
+        testMatch: ["**.test.ts"],
         retries: 0,     //*whatever test failed it will rerun that failed test*//
         reporter: [["dot"],["json" , {outputFile: "test-result.json"}]]
 
