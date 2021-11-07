0c42f5b8f0a5984c0558abbb815a666a009c92a2
jayakrishna2398 <jayakrishnakrish1998@gmail.com>
2021-11-07 11:39:45 +0530

    tried to use selectors using layouts

--- visual-comparision/visual.test.ts
+++ visual-comparision/visual.test.ts
@@ -2,6 +2,7 @@ import{test,expect} from "@playwright/test";
 
 test("visual comparision", async({page})=>{
     await page.goto("https://demoblaze.com/")
+    await page.pause();
     expect (await page.screenshot({
         fullPage: true
     })).toMatchSnapshot("demoblaze.png")
