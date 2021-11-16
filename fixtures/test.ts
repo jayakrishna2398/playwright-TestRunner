36c468e9aeddc0c5949ac32e05e3b66478c4cf00
jayakrishna2398 <jayakrishnakrish1998@gmail.com>
2021-11-16 17:23:11 +0530

    UPDATE: Implemented skip tests failure and step

--- fixtures/test.ts
+++ fixtures/test.ts
@@ -1,9 +1,11 @@
 import test,{expect} from "./basePages"
 
 import * as data from "../test/data/login.cred.json";
-        test.describe("Test grouping", ()=>{
 
-
+        //test.describe.serial.only("", () =>{})    //*This is used to test only one group of tests*//
+        //test.describe.serial("Test grouping", ()=>{      //this is called as serial where if the 1st test case gets failed, then the other gets skipped//
+        test.describe("Test grouping", ()=>{ 
+                
 test("test 1", async({commonFunctions,headerPage,loginPage,page})=>{
         await page.goto("https://letcode.in/")
         await headerPage.clickLoginLink();
@@ -15,6 +17,19 @@ test("test 1", async({commonFunctions,headerPage,loginPage,page})=>{
         expect(await toaster?.textContent()).toContain(" Welcome Jayakrishna Thirunavarrasu ");
         await headerPage.clickSignOutLink();
 })
+
+// await test.step("the fails scenario", async() =>{  //It is a end to end testing.If the test fails in any operation, then it gets broked.
+//         await test.step("Login", async()=>{
+//                 await page.goto("https://letcode.in/")
+//                 await headerPage.clickLoginLink();
+//                 expect(page.url()).toBe("https://letcode.in/signin")
+//                 await loginPage.enterUserName(data.email);
+//                 await loginPage.enterPassword("wrong");
+//                 await loginPage.clickBtn();
+//                 const toaster = await commonFunctions.toaster;
+//                 expect(await toaster?.textContent()).toContain(" Welcome Jayakrishna Thirunavarrasu ");
+//                 await headerPage.clickSignOutLink();
+// })
 
 test("test2", async({page})=> {
         await page.goto("https://demoblaze.com/");
--- playwright.config.ts
+++ playwright.config.ts
@@ -14,9 +14,9 @@ const config: PlaywrightTestConfig = {
         //grep:[new RegExp("@smoke"), new RegExp("@reg")],   //It will execute @smoke and @reg in tag.test.ts
         //grepInvert:[new RegExp("@smoke")],            //It will execute other than @smoke tests
         
-        testMatch: ["loginApi.test.ts"],
+        testMatch: ["test.ts"],
         retries: 0,     //*whatever test failed it will rerun that failed test*//
-        reporter: [["list"],["json" , {outputFile: "test-result.json"}]]
+        reporter: [["list"],["json" , {outputFile: "test-result.json"}],["experimental-allure-playwright"]]
 
 }
 export default config;
