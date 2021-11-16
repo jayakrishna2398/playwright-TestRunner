0158840b8d463b789b4f6b714eeccf6a3ca6c52d
jayakrishna2398 <jayakrishnakrish1998@gmail.com>
2021-11-16 10:42:15 +0530

    NEW: api response

--- Api response/loginApi.test.ts
+++ Api response/loginApi.test.ts
@@ -1,4 +1,5 @@
 import {test} from "@playwright/test";
+import * as data from "./login.cred.json";
 
 test("API login response check", async({page})=>{
     await page.goto("https://letcode.in/signin");
@@ -10,8 +11,8 @@ test("API login response check", async({page})=>{
             res.url() == "https://letcode.in/signin"
             
         ),
-        await page.fill("input[name='email']", "jayakrishna2398@gmail.com"),
-        await page.fill("input[placeholder='Enter password']", ""),
+        await page.fill("input[name='email']", data.email),
+        await page.fill("input[placeholder='Enter password']", data.pass),
         await page.click("//button[text()='LOGIN']")
         ])
         console.log(await response.json());
