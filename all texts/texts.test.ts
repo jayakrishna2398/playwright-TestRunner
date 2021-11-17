2167ceef4e1e8672cca5c04c4f5e50b66b42397e
jayakrishna2398 <jayakrishnakrish1998@gmail.com>
2021-11-17 11:14:47 +0530

    UPDATE: Done innerHTML vs innerText vs textContent

--- all texts/texts.test.ts
+++ all texts/texts.test.ts
@@ -3,18 +3,18 @@ import {test} from "@playwright/test"
 test("All texts difference", async({page})=>{
     await page.goto("file:///E:/playwright%20TestRunner/all%20texts/index.html");
     await test.step("Inner HTML", async()=>{            
-        await page.click("#innerHTML")
+        await page.click("#innerHTML")                    //o/p:Inner HTML is: hey <strong>there</strong> <span>Am I visible?</span>
         const txt =await page.locator("#attach").innerHTML()
         console.log("Inner HTML is:", txt);
 
     })
     await test.step("Inner text HTML", async()=>{       
-        await page.click("#innerHTML")
+        await page.click("#innerHTML")               //o/p:hey there
         const txt = await page.locator("#attach").innerText()
         console.log("Inner text is:", txt);
     })
     await test.step("Text context" , async()=>{        
-       await page.click("#innerHTML");
+       await page.click("#innerHTML");              //o/p:hey there Am I visible?
         const txt = await page.locator("#attach").textContent()
         console.log("Text content is:", txt)
     })
@@ -23,4 +23,4 @@ test("All texts difference", async({page})=>{
 
 //InnerHTML(): It will give whatever is present in the DOM including the tags,everything
 //InnerText(): Inner text means, it will return only visible elements present in DOM
-//TextContext(): Text content means,it will return all the elements whether its visible or not in DOM
+//TextContext(): Text content means,it will return all the elements(string or numbers) whether its visible or not in DOM(except tags)
